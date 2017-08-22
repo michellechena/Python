1be8d9f162106afa0962b9733acf7a53dbcccc99
Michelle Chen <michellechena@github.com>
2017-08-21 22:24:02 -0700

    Numbers https://docs.python.org/3/tutorial/introduction.html#numbers

---
+++ Numbers/Numbers/Numbers.py
@@ -0,0 +1,36 @@
+# https://docs.python.org/3/tutorial/introduction.html#numbers
+# Operators: +, -, *, /
+# () as grouping
+
+'''
+/ always returns a floati
+// returns integer portion
+% remainder
+** power
+= assign a value to a variable
+'''
+
+
+num = 17 / 3
+num1 = 17 // 3
+num2 = 17 % 3
+num3 = 5 * 3 + 2
+num4 = -5 * 3 - 4
+
+width = 10
+height = 20
+area = width * height
+powernum = 3 ** 2
+
+
+# full support for floating, convert integer to float
+floatnum = 4 * 3.75 - 1   #14.0
+
+# In interactive mode, the last printed expression is assigned to the variable _, 
+# it seems that visual studio does not support interactive mode. It gives me a build error
+#tax = 12.5/100
+#price = 100.50
+#price * tax
+#price + _
+#round(_, 2)
+
