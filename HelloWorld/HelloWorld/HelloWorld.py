d4c951d1ed81e4f1c72f8b581dd16d7a52b25409
Michelle Chen <michellechena@github.com>
2017-08-21 21:59:02 -0700

    Hello World (comments, import datetime and print)

---
+++ HelloWorld/HelloWorld/HelloWorld.py
@@ -0,0 +1,25 @@
+
+#https://docs.python.org/3.6/
+# single line comment
+
+"""
+multiple line1
+line2
+"""
+
+'''
+multiple line comment line 1
+line2
+'''
+
+import datetime
+
+print ("hello world" )
+
+print ("hello world from Seattle")
+
+print (datetime.datetime.now())
+
+print ("hello world from SpaceNeedle " + str(datetime.datetime.now()))
+
+
